f1654bc8dfea497133af3c4c3f9afa64e3445f4f
Benedek Racz <betontalpfa@gmail.com>
2020-01-25 16:30:39 +0100

    [CLN] make wexpect subdir

---
+++ wexpect/__init__.py
@@ -0,0 +1,9 @@
+from .legacy_wexpect import ExceptionPexpect
+from .legacy_wexpect import EOF
+from .legacy_wexpect import TIMEOUT
+from .legacy_wexpect import spawn
+from .legacy_wexpect import run
+from .legacy_wexpect import split_command_line
+from .legacy_wexpect import __version__
+
+__all__ = ['ExceptionPexpect', 'EOF', 'TIMEOUT', 'spawn', 'run', 'split_command_line', '__version__']
