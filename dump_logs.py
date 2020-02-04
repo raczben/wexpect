4fc226b3d9f1516d576316e419762fbdda6186ee
Benedek Racz <betontalpfa@gmail.com>
2020-02-04 09:34:00 +0100

    minimal 11

--- dump_logs.py
+++ dump_logs.py
@@ -1,9 +1,21 @@
 import os
 import glob
 
+read_files = glob.glob("*.log")
+
+for f in read_files:
+    print()
+    print(f)
+    print()
+    with open(f, "r") as infile:
+        print(infile.read())
+
 read_files = glob.glob(".wlog/*.log")
 
 for f in read_files:
+    print()
+    print(f)
+    print()
     with open(f, "r") as infile:
         print(infile.read())
         
@@ -11,5 +23,8 @@ for f in read_files:
 read_files = glob.glob("tests/.wlog/*.log")
 
 for f in read_files:
+    print()
+    print(f)
+    print()
     with open(f, "rb") as infile:
         print(infile.read())
